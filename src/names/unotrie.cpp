efc330aeee7a49e404c89efb28d9b00027dd0970
Daniel Kraft <d@domob.eu>
2015-04-02 21:50:49 +0200

    Turn UNO trie into a radix trie.
    Use a new 'prefix' variable to encode long, single-child strings in the
    trie.  The hash is still computed in the same way as before -- i. e.,
    the prefix is expanded for that.

--- src/names/unotrie.cpp
+++ src/names/unotrie.cpp
@@ -1,11 +1,21 @@
-// Copyright (c) 2014-2015 Daniel Kraft
+// Copyright (c) 2015 Daniel Kraft
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "names/unotrie.h"
 
+#include "hash.h"
+
+#include <algorithm>
+#include <utility>
+
 /* ************************************************************************** */
 /* CUnoTrie.  */
+
+CUnoTrie::~CUnoTrie ()
+{
+  Clear ();
+}
 
 void
 CUnoTrie::Clear ()
@@ -19,7 +29,102 @@ CUnoTrie::Clear ()
   children.clear ();
 }
 
-CUnoTrie::~CUnoTrie ()
+uint256
+CUnoTrie::GetHash () const
 {
-  Clear ();
+  std::map<unsigned char, uint256> childHashes;
+  BOOST_FOREACH(PAIRTYPE(unsigned char, const CUnoTrie*) child, children)
+    childHashes.insert (std::make_pair (child.first, child.second->GetHash ()));
+
+  /* Compute base hash.  This is the hash without the prefix.  I. e.,
+     the hash of the "lowest" trie node, which actually holds all the
+     data and the children present in the object.  */
+  uint256 res;
+  {
+    CHashWriter hasher(SER_GETHASH, PROTOCOL_VERSION);
+    const bool hasData = (data != NULL);
+    hasher << hasData;
+    if (hasData)
+      hasher << *data;
+    hasher << childHashes;
+    res = hasher.GetHash ();
+  }
+
+  /* Follow up the prefix, if there is any.  */
+  valtype::const_reverse_iterator i;
+  for (i = prefix.rbegin (); i != prefix.rend (); ++i)
+    {
+      CHashWriter hasher(SER_GETHASH, PROTOCOL_VERSION);
+      hasher << false;
+
+      childHashes.clear ();
+      childHashes.insert (std::make_pair (*i, res));
+      hasher << childHashes;
+
+      res = hasher.GetHash ();
+    }
+
+  return res;
+}
+
+void
+CUnoTrie::Insert (valtype::const_iterator a, const valtype::const_iterator& b,
+                  const CNameData& d)
+{
+  /* Follow the prefix as far as possible.  */
+  valtype::const_iterator i = prefix.begin ();
+  while (i != prefix.end () && a != b && *i == *a)
+    {
+      ++i;
+      ++a;
+    }
+
+  /* If we have not yet reached the end of the prefix, we have to split
+     it and insert a new intermediate node.  In fact, the current node
+     is turned into the intermediate one (so that upstream pointers
+     are preserved).  */
+  if (i != prefix.end ())
+    {
+      std::auto_ptr<CUnoTrie> newChild;
+      newChild.reset (new CUnoTrie (i + 1, prefix.end ()));
+
+      assert (!newChild->data);
+      std::swap (data, newChild->data);
+
+      assert (newChild->children.empty ());
+      newChild->children.insert (std::make_pair (*i, newChild.get ()));
+      children.swap (newChild->children);
+      newChild.release ();
+
+      const valtype::size_type newSize = i - prefix.begin ();
+      assert (newSize < prefix.size ());
+      prefix.resize (newSize);
+
+      // Fall through.
+    }
+
+  /* Now insert into the child array.  */
+  assert (i == prefix.end ());
+  if (a == b)
+    {
+      if (data)
+        delete data;
+      data = new CNameData (d);
+    }
+  else
+    {
+      const unsigned char nextByte = *a;
+      std::map<unsigned char, CUnoTrie*>::iterator mi;
+      mi = children.find (nextByte);
+
+      ++a;
+      if (mi == children.end ())
+        {
+          std::auto_ptr<CUnoTrie> newChild;
+          newChild.reset (new CUnoTrie (a, b, new CNameData (d)));
+          children.insert (std::make_pair (nextByte, newChild.release ()));
+        }
+      else
+        mi->second->Insert (a, b, d);
+    }
 }
--- src/names/unotrie.h
+++ src/names/unotrie.h
@@ -1,11 +1,10 @@
-// Copyright (c) 2014-2015 Daniel Kraft
+// Copyright (c) 2015 Daniel Kraft
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef H_BITCOIN_NAMES_UNOTRIE
 #define H_BITCOIN_NAMES_UNOTRIE
 
-#include "hash.h"
 #include "names/common.h"
 #include "serialize.h"
 #include "uint256.h"
@@ -32,6 +31,12 @@ class CUnoTrie
 
 private:
 
+  /**
+   * Additional bytes to add along the edge from this node to the
+   * first character in the parent's map.
+   */
+  valtype prefix;
+
   /** Data of the name corresponding to this node (if any).  */
   CNameData* data;
 
@@ -39,9 +44,20 @@ private:
   std::map<unsigned char, CUnoTrie*> children;
 
   /**
-   * Release all memory stored.  This is used both from the destructor
-   * and also when unserialising (to free the old state, if any).
-   * Thus shared here.
+   * Construct with given prefix range and data.  This is used
+   * when inserting nodes into the trie.
+   * @param a Prefix start.
+   * @param b Prefix end.
+   * @param d Data to insert (if any).
+   */
+  inline CUnoTrie (const valtype::const_iterator& a,
+                   const valtype::const_iterator& b, CNameData* d = NULL)
+    : prefix(a, b), data(d), children()
+  {}
+
+  /**
+   * Clear all memory.  This is used for unserialisation and for the destructor,
+   * thus it is shared here.
    */
   void Clear ();
 
@@ -51,7 +67,7 @@ public:
    * Construct it with empty data and no children.
    */
   inline CUnoTrie ()
-    : data(NULL), children()
+    : prefix(), data(NULL), children()
   {}
 
   /**
@@ -60,108 +76,76 @@ public:
   virtual ~CUnoTrie ();
 
   /**
-   * Get root hash of this node (including the full subtree).
+   * Get root hash of this node (including the full subtree).  This also
+   * includes the prefix.  I. e., the hash is actually of the node that
+   * would correspond to the "parent" at the beginning of the prefix.
+   *
+   * Note that the hash itself is computed as if there were no prefixes
+   * and instead an ordinary trie was used.  This is done to make the
+   * consensus rules simple to implement in alternative situations.
+   *
    * @return The node's subtree's hash.
    */
-  inline uint256
-  GetHash () const
-  {
-    return SerializeHash (*this);
-  }
+  uint256 GetHash () const;
 
   /**
    * Insert the given data in the subtree at the given position.
    * If there is already data at the position, it is replaced.
-   * There must not yet be data at this position.
    * @param a Start of the prefix.
    * @param b End of the prefix.
    * @param d Data of the name to put there.
    */
-  template<typename Iter>
-    void
-    Insert (const Iter& a, const Iter& b, const CNameData& d)
-  {
-    if (a == b)
-      {
-        if (data)
-          delete data;
-        data = new CNameData (d);
-        return;
-      }
+  void Insert (valtype::const_iterator a, const valtype::const_iterator& b,
+               const CNameData& d);
 
-    const unsigned char nextByte = *a;
-    std::map<unsigned char, CUnoTrie*>::iterator mi = children.find (nextByte);
-    if (mi != children.end ())
-      mi->second->Insert (a + 1, b, d);
-    else
-      {
-        std::auto_ptr<CUnoTrie> child(new CUnoTrie ());
-        child->Insert (a + 1, b, d);
-        children.insert (std::make_pair (nextByte, child.release ()));
-      }
+  /* Implement serialisation.  This is *not* used for hashing!  Hashing
+     is done by GetHash in an ad-hoc fashion, since it does resolve
+     the prefix explicitly.  */
+
+  template<typename Stream>
+    void
+    Serialize (Stream& s, int nType, int nVersion) const
+  {
+    assert (!(nType & SER_GETHASH));
+
+    s << prefix << static_cast<bool> (data != NULL);
+    if (data)
+      s << *data;
+
+    WriteCompactSize (s, children.size ());
+    BOOST_FOREACH (const PAIRTYPE(unsigned char, CUnoTrie*)& child, children)
+      s << child.first << *child.second;
   }
 
-  /* Implement serialisation.  Note that the "get hash" serialisation
-     uses the hashes of all children (as in a Merkle tree) instead
-     of the fully serialised children themselves.  */
-
-  ADD_SERIALIZE_METHODS;
-
-  template<typename Stream, typename Operation>
-    inline void SerializationOp (Stream& s, Operation ser_action,
-                                 int nType, int nVersion)
+  template<typename Stream>
+    void
+    Unserialize (Stream& s, int nType, int nVersion)
   {
-    if (ser_action.ForRead ())
-      {
-        Clear ();
-        assert (!data);
-        assert (children.empty ());
-      }
+    Clear ();
+    s >> prefix;
 
     bool fHasData;
-    if (!ser_action.ForRead ())
-      fHasData = (data != NULL);
-    READWRITE (fHasData);
-
+    s >> fHasData;
+    assert (!data);
     if (fHasData)
-      READWRITE (*data);
+      {
+        data = new CNameData ();
+        s >> *data;
+      }
 
-    uint16_t sz;
-    if (!ser_action.ForRead ())
-      sz = children.size ();
-    READWRITE (sz);
+    const uint64_t sz = ReadCompactSize (s);
+    assert (children.empty ());
+    for (unsigned i = 0; i < sz; ++i)
+      {
+        unsigned char nextByte;
+        std::auto_ptr<CUnoTrie> child(new CUnoTrie ());
+        s >> nextByte >> *child;
 
-    /* We are reading, fill in entries one by one.  */
-    if (ser_action.ForRead ())
-      for (unsigned i = 0; i < sz; ++i)
-        {
-          unsigned char nextByte;
-          std::auto_ptr<CUnoTrie> child(new CUnoTrie ());
-
-          READWRITE (nextByte);
-          READWRITE (*child);
-
-          if (children.count (nextByte) > 0)
-            throw std::runtime_error ("duplicate child character during"
-                                      " unserialisation of CUnoTrie");
-          children.insert (std::make_pair (nextByte, child.release ()));
-        }
-
-    /* We are writing, iterate over the map.  */
-    else
-      BOOST_FOREACH (const PAIRTYPE(unsigned char, CUnoTrie*)& child, children)
-        {
-          const unsigned char nextByte = child.first;
-          READWRITE (nextByte);
-
-          if (nType & SER_GETHASH)
-            {
-              const uint256 hash = child.second->GetHash ();
-              READWRITE (hash);
-            }
-          else
-            READWRITE (*child.second);
-        }
+        if (children.count (nextByte) > 0)
+          throw std::runtime_error ("duplicate child character during"
+                                    " unserialisation of CUnoTrie");
+        children.insert (std::make_pair (nextByte, child.release ()));
+      }
   }
 
 };
